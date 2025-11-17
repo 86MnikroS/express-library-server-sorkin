cebeea0a692ba2bc95610f36462eb950478fc7cc
86MnikroS <sorkin.mihail@gmail.com>
2025-11-17 18:51:46 +0200

    new branch for cw11 + validation

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { bookController } from "../controllers/BookController.ts";
+import {bodyValidator} from "../middleware/bodyValidator.ts";
+import {bookJoiSchema, readerJoiSchema} from "../utils/joiSchemas.ts";
 
 export const bookRouter = Router();
 
@@ -10,13 +12,13 @@ bookRouter.get("/", bookController.getAllBooks);
 bookRouter.get("/author/:author", bookController.getBookByAuthor);
 
 // POST /books
-bookRouter.post("/", bookController.addBook);
+bookRouter.post("/", bodyValidator(bookJoiSchema), bookController.addBook);
 
 // DELETE /books/:id
 bookRouter.delete("/:id", bookController.removeBook);
 
 // POST /books/:id/pick
-bookRouter.post("/:id/pick", bookController.pickBook);
+bookRouter.post("/:id/pick", bodyValidator(readerJoiSchema), bookController.pickBook);
 
 // POST /books/:id/return
 bookRouter.post("/:id/return", bookController.returnBook);
